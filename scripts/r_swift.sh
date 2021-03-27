e919c024a060f4a8cf85e8dff05c924d7a7f9ebb
tokizuoh <k_sakai@yumemi.co.jp>
2021-03-27 22:34:13 +0900

    R.swiftを導入した

---
+++ scripts/r_swift.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+if mint list | grep -q 'R.swift'; then
+  mint run R.swift rswift generate "$SRCROOT/R.generated.swift"
+else
+  echo "error: R.swift not installed; run 'mint bootstrap' to install"
+  return -1
+fi
