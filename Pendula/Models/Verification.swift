d63a6ef073ba32d48888a93742f73ec5a7318003
tokizuoh <k_sakai@yumemi.co.jp>
2021-03-28 12:34:35 +0900

    一覧画面の枠組みを追加

---
+++ Pendula/Models/Verification.swift
@@ -0,0 +1,12 @@
+//
+//  Verification.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/03/28.
+//
+
+import Foundation
+
+struct Verification {
+    let title: String
+}
--- Pendula/VerificationList/VerificationListViewController.swift
+++ Pendula/VerificationList/VerificationListViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 final class VerificationListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+
+    var viewModel: [Verification]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // TODO: 実データに変更する
+        viewModel = [
+            Verification(title: "a"),
+            Verification(title: "bbbbbbbbbbbbbbb"),
+            Verification(title: "ccccccccccc")
+        ]
+    }
+
+}
+
+extension VerificationListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: カスタムセルを使用する
+        let cell = UITableViewCell()
+        cell.textLabel?.text = viewModel?[indexPath.row].title
+        return cell
     }
 
 }
