c174a63580c3e646a8521ebff854dbbc7af97dc5
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-10 16:39:20 +0900

    [#47] 図形を表示処理を共通化した

--- Pendula/View/Component/003_BezierPathScene/BezierPathViewController.swift
+++ Pendula/View/Component/003_BezierPathScene/BezierPathViewController.swift
@@ -18,12 +18,50 @@ final class BezierPathViewController: ComponentBaseViewController {
         }
     }
 
+    struct ViewModel {
+        let tital: String
+        let drawHandler: (_ rect: CGRect) -> Void
+    }
+
+    var viewModels: [ViewModel]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationItem(navigationTitle: "003_BezierPath",
                                 blogURL: nil,
                                 githubPRURL: nil)
+        configureViewModels()
+    }
+
+}
+
+extension BezierPathViewController {
+
+    private func configureViewModels() {
+        viewModels = [
+            ViewModel(tital: "三角形",
+                      drawHandler: { rect in
+                        let path = UIBezierPath()
+                        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+                        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY))
+                        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+                        path.close()
+                        UIColor.red.setStroke()
+                        path.stroke()
+                      }),
+            ViewModel(tital: "ひし形",
+                      drawHandler: { rect in
+                        let path = UIBezierPath()
+                        path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+                        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY / 2))
+                        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+                        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY + rect.midY / 2))
+                        path.close()
+                        UIColor.red.setStroke()
+                        path.stroke()
+                      })
+        ]
     }
 
 }
@@ -31,11 +69,18 @@ final class BezierPathViewController: ComponentBaseViewController {
 extension BezierPathViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return viewModels?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let viewModels = viewModels,
+              viewModels.indices.contains(indexPath.row) else {
+            return UITableViewCell()
+        }
+
         let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.bezierPathTableViewCell, for: indexPath)!
+        cell.titleLabel.text = viewModels[indexPath.row].tital
+        cell.drawHandler = viewModels[indexPath.row].drawHandler
         return cell
     }
 
--- Pendula/View/Component/003_BezierPathScene/CustomBezierPathView.swift
+++ Pendula/View/Component/003_BezierPathScene/CustomBezierPathView.swift
@@ -9,14 +9,14 @@ import UIKit
 
 @IBDesignable final class CustomBezierPathView: UIView {
 
+    var drawHandler: ((_ rect: CGRect) -> Void)? {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+
     override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
-        path.close()
-        UIColor.red.setStroke()
-        path.stroke()
+        drawHandler?(rect)
     }
 
 }
