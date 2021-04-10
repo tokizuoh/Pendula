8e537948b8579bbe93c096bf1807a5fa6f72ef92
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-10 13:30:28 +0900

    [#47] BezierPathコンポーネントの枠組みを追加

---
+++ Pendula/View/Component/003_BezierPathScene/BezierPathViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BezierPathViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/04/10.
+//
+
+import UIKit
+
+final class BezierPathViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigationItem(navigationTitle: "003_BezierPath",
+                                blogURL: nil,
+                                githubPRURL: nil)
+    }
+
+}
