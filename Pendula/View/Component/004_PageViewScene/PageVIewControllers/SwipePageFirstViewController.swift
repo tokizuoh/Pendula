a84aca2174c21b4cf88a7716028b86fa5a0d73d5
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-14 21:39:10 +0900

    [#55] ContainerViewに入れるViewControllerを追加

---
+++ Pendula/View/Component/004_PageViewScene/PageVIewControllers/SwipePageFirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SwipePageFirstViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/04/14.
+//
+
+import UIKit
+
+final class SwipePageFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
