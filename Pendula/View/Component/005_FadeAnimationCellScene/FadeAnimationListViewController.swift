f811d6f9680132c736a6d861facf4d7b19b368ec
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-17 09:52:29 +0900

    [#58] 005コンポーネントの枠組み・遷移を追加

---
+++ Pendula/View/Component/005_FadeAnimationCellScene/FadeAnimationListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FadeAnimationListViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/04/17.
+//
+
+import UIKit
+
+final class FadeAnimationListViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigationItem(navigationTitle: "005 CellAnimation",
+                                blogURL: nil,
+                                githubPRURL: nil)
+    }
+
+}
