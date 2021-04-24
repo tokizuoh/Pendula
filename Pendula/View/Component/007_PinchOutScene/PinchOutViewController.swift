14abd45380559bd9222747936c52df2b42000016
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-24 19:28:47 +0900

    [#64] タップ時の処理の雛形を追加

--- Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
+++ Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
@@ -9,12 +9,52 @@ import UIKit
 
 final class PinchOutViewController: ComponentBaseViewController {
 
+    var startPoint: CGPoint?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationItem(navigationTitle: "007 PinchOut",
                                 blogURL: nil,
                                 githubPRURL: nil)
+        configureGesture()
+    }
+
+}
+
+// MARK: - UIGestureRecognizer
+extension PinchOutViewController {
+
+    private func configureGesture() {
+        let singleTapGesture = UITapGestureRecognizer(target: self,
+                                                      action: #selector(detectSingleTap(sender:)))
+        singleTapGesture.numberOfTapsRequired = 1
+        view.addGestureRecognizer(singleTapGesture)
+    }
+
+    @objc private func detectSingleTap(sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location.x, location.y)
+
+        if let startPoint = startPoint {
+            // Animation
+        } else {
+            // Animation
+            self.startPoint = location
+        }
+    }
+
+}
+
+// MARK: - Animation
+extension PinchOutViewController {
+
+    private func beginAnimation() {
+
+    }
+
+    private func finishAnimation() {
+
     }
 
 }
