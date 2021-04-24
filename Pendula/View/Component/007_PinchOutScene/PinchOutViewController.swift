c7329f92e8074080e925c936fc0df08e9faf567b
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-24 20:17:43 +0900

    [#64] アニメーション処理を追加

--- Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
+++ Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class PinchOutViewController: ComponentBaseViewController {
 
     var startPoint: CGPoint?
+    var startPointView: UIView?
+    var finishPointView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,9 +39,11 @@ extension PinchOutViewController {
         print(location.x, location.y)
 
         if let startPoint = startPoint {
-            // Animation
+            finishAnimation(startPoint: startPoint,
+                            endPoint: location)
+            self.startPoint = nil
         } else {
-            // Animation
+            beginAnimation(location: location)
             self.startPoint = location
         }
     }
@@ -49,12 +53,45 @@ extension PinchOutViewController {
 // MARK: - Animation
 extension PinchOutViewController {
 
-    private func beginAnimation() {
-
+    private func beginAnimation(location: CGPoint) {
+        startPointView = UIView(frame: CGRect(x: location.x,
+                                              y: location.y,
+                                              width: 20,
+                                              height: 20))
+        startPointView?.backgroundColor = .red
+        view.addSubview(startPointView!)
     }
 
-    private func finishAnimation() {
+    private func finishAnimation(startPoint: CGPoint, endPoint: CGPoint) {
+        func generateSquareView(startPoint: CGPoint, endPoint: CGPoint) {
+            let width = endPoint.x - startPoint.x
+            let height = endPoint.y - startPoint.y
+            let squareView = UIView(frame: CGRect(x: min(startPoint.x, endPoint.x),
+                                                  y: min(startPoint.y, endPoint.y),
+                                                  width: abs(width),
+                                                  height: abs(height)))
+            squareView.backgroundColor = UIColor(hue: CGFloat.random(in: 0...1.0),
+                                                 saturation: 100/100,
+                                                 brightness: 100/42,
+                                                 alpha: 0.5)
+            squareView.alpha = 0.3
+            view.addSubview(squareView)
+        }
 
+        finishPointView = UIView(frame: CGRect(x: endPoint.x,
+                                               y: endPoint.y,
+                                               width: 20,
+                                               height: 20))
+        finishPointView?.backgroundColor = .blue
+        view.addSubview(finishPointView!)
+
+        generateSquareView(startPoint: startPoint,
+                           endPoint: endPoint)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [unowned self] in
+            self.startPointView?.removeFromSuperview()
+            self.finishPointView?.removeFromSuperview()
+        }
     }
 
 }
