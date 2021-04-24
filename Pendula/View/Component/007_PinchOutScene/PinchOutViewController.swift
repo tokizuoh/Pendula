58c781d9e680cb051d3ebd18991d60eeb4e972a1
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-24 20:35:29 +0900

    [#64] アニメーションの調整とUIViewの削除処理を追加

--- Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
+++ Pendula/View/Component/007_PinchOutScene/PinchOutViewController.swift
@@ -13,13 +13,15 @@ final class PinchOutViewController: ComponentBaseViewController {
     var startPointView: UIView?
     var finishPointView: UIView?
 
+    var squareViews: [UIView] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationItem(navigationTitle: "007 PinchOut",
                                 blogURL: nil,
                                 githubPRURL: nil)
-        configureGesture()
+        configureGestures()
     }
 
 }
@@ -27,16 +29,20 @@ final class PinchOutViewController: ComponentBaseViewController {
 // MARK: - UIGestureRecognizer
 extension PinchOutViewController {
 
-    private func configureGesture() {
+    private func configureGestures() {
         let singleTapGesture = UITapGestureRecognizer(target: self,
                                                       action: #selector(detectSingleTap(sender:)))
         singleTapGesture.numberOfTapsRequired = 1
         view.addGestureRecognizer(singleTapGesture)
+
+        let swipeGesture = UISwipeGestureRecognizer(target: self,
+                                                    action: #selector(detectSwipeGesture))
+        swipeGesture.direction = .down
+        view.addGestureRecognizer(swipeGesture)
     }
 
     @objc private func detectSingleTap(sender: UITapGestureRecognizer) {
         let location = sender.location(in: view)
-        print(location.x, location.y)
 
         if let startPoint = startPoint {
             finishAnimation(startPoint: startPoint,
@@ -48,6 +54,10 @@ extension PinchOutViewController {
         }
     }
 
+    @objc private func detectSwipeGesture() {
+        _ = squareViews.map { $0.removeFromSuperview() }
+        self.squareViews.removeAll()
+    }
 }
 
 // MARK: - Animation
@@ -70,14 +80,16 @@ extension PinchOutViewController {
                                                   y: min(startPoint.y, endPoint.y),
                                                   width: abs(width),
                                                   height: abs(height)))
-            squareView.backgroundColor = UIColor(hue: CGFloat.random(in: 0...1.0),
+            squareView.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1.0),
                                                  saturation: 100/100,
                                                  brightness: 100/42,
                                                  alpha: 0.5)
             squareView.alpha = 0.3
             view.addSubview(squareView)
+            squareViews.append(squareView)
         }
 
+        view.isUserInteractionEnabled = false
         finishPointView = UIView(frame: CGRect(x: endPoint.x,
                                                y: endPoint.y,
                                                width: 20,
@@ -88,9 +100,10 @@ extension PinchOutViewController {
         generateSquareView(startPoint: startPoint,
                            endPoint: endPoint)
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [unowned self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [unowned self] in
             self.startPointView?.removeFromSuperview()
             self.finishPointView?.removeFromSuperview()
+            self.view.isUserInteractionEnabled = true
         }
     }
 
