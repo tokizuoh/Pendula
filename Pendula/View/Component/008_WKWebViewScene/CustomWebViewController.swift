65cde1c668a8c7ae5eeaf022077f68bff65f3678
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-28 09:59:51 +0900

    [#67] ページ読み込み終了までUIActivityIndicatorを出すようにした

--- Pendula/View/Component/008_WKWebViewScene/CustomWebViewController.swift
+++ Pendula/View/Component/008_WKWebViewScene/CustomWebViewController.swift
@@ -11,7 +11,7 @@ import WebKit
 final class CustomWebViewController: ComponentBaseViewController {
 
     @IBOutlet weak var webView: WKWebView!
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     // TODO [#67]: 取得したhtmlのなにかを取得する
     @IBOutlet weak var dummyLabel: UILabel! {
         didSet {
@@ -25,18 +25,42 @@ final class CustomWebViewController: ComponentBaseViewController {
         configureNavigationItem(navigationTitle: "008 WebView",
                                 blogURL: nil,
                                 githubPRURL: nil)
+        configureActivityIndicator()
         configureWebView()
     }
 
 }
 
+// MARK: - WKWebView
 extension CustomWebViewController {
 
-    // TODO [#67]: 画面読み込み時にちらつきがあるのでindicatorを入れる
     private func configureWebView() {
+        webView.navigationDelegate = self
         let url = URL(string: "https://tokizuoh.dev/")
         let request = URLRequest(url: url!)
         webView.load(request)
+    }
+
+}
+
+// MARK: - UIActivityIndicator
+extension CustomWebViewController {
+
+    private func configureActivityIndicator() {
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+    }
+
+}
+
+// MARK: - WKNavigationDelegate
+extension CustomWebViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [unowned self] in
+            self.activityIndicator.stopAnimating()
+            decisionHandler(.allow)
+        }
     }
 
 }
