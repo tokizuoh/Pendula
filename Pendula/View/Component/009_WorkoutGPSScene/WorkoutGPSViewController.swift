959475e6b85364f73e68579ed85939da1afb8d5d
tokizuoh <k_sakai@yumemi.co.jp>
2021-05-23 19:24:38 +0900

    [#70] 009コンポーネントの枠組みと遷移を追加

---
+++ Pendula/View/Component/009_WorkoutGPSScene/WorkoutGPSViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutGPSViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/05/23.
+//
+
+import UIKit
+
+final class WorkoutGPSViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigationItem(navigationTitle: "009 Workout GPS",
+                                blogURL: nil,
+                                githubPRURL: nil)
+    }
+
+}
