61e6a510ee73f2e94fb61bf710806df8ede9e95b
tokizuoh <k_sakai@yumemi.co.jp>
2021-05-27 22:22:51 +0900

    [#71] UI要素のつなぎこみを追加

--- Pendula/View/Component/009_WorkoutCyclingScene/WorkoutCyclingViewController.swift
+++ Pendula/View/Component/009_WorkoutCyclingScene/WorkoutCyclingViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 final class WorkoutCyclingViewController: ComponentBaseViewController {
 
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var goButton: UIButton! {
+        didSet {
+            goButton.layer.cornerRadius = 4.0
+        }
+    }
+
+    struct ViewModel {
+        var startDate: Date
+        var endDate: Date
+    }
+
+    var viewModel: ViewModel? {
+        didSet {
+            guard let viewModel = viewModel else {
+                return
+            }
+            startDateLabel.text = viewModel.startDate.string(format: .yyyyMMddPd)
+            endDateLabel.text = viewModel.endDate.string(format: .yyyyMMddPd)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
