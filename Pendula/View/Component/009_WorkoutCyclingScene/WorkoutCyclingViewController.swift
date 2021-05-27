ee479df1a37a8bf992935cd97ed6ff6efc4aacd8
tokizuoh <k_sakai@yumemi.co.jp>
2021-05-27 22:52:53 +0900

    [#71] サイクリングのワークアウトを取得する処理を追加

--- Pendula/View/Component/009_WorkoutCyclingScene/WorkoutCyclingViewController.swift
+++ Pendula/View/Component/009_WorkoutCyclingScene/WorkoutCyclingViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import HealthKit
 
 final class WorkoutCyclingViewController: ComponentBaseViewController {
 
@@ -21,14 +22,29 @@ final class WorkoutCyclingViewController: ComponentBaseViewController {
         var startDate: Date
         var endDate: Date
     }
-
     var viewModel: ViewModel? {
         didSet {
             guard let viewModel = viewModel else {
                 return
             }
-            startDateLabel.text = viewModel.startDate.string(format: .yyyyMMddPd)
-            endDateLabel.text = viewModel.endDate.string(format: .yyyyMMddPd)
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                self.startDateLabel.text = viewModel.startDate.string(format: .yyyyMMddPd)
+                self.endDateLabel.text = viewModel.endDate.string(format: .yyyyMMddPd)
+            }
+        }
+    }
+    var healthStore: HKHealthStore?
+    var workouts: [HKWorkout]? {
+        didSet {
+            guard let workouts = workouts,
+                  workouts.count > 0 else {
+                return
+            }
+            viewModel = ViewModel(startDate: workouts.first!.startDate,
+                                  endDate: workouts.last!.startDate)
         }
     }
 
@@ -38,6 +54,45 @@ final class WorkoutCyclingViewController: ComponentBaseViewController {
         configureNavigationItem(navigationTitle: "009 WorkoutCycling",
                                 blogURL: nil,
                                 githubPRURL: nil)
+        configureHelathStore()
+    }
+
+}
+
+// MARK: - HealthKit
+extension WorkoutCyclingViewController {
+
+    private func configureHelathStore() {
+        healthStore = HKHealthStore()
+        let allTypes = Set([
+            HKObjectType.workoutType()
+        ])
+        healthStore?.requestAuthorization(toShare: nil, read: allTypes) { success, _ in
+            guard success else {
+                return
+            }
+            self.getWorkouts()
+        }
+    }
+
+    private func getWorkouts() {
+        let type = HKWorkoutType.workoutType()
+        let predicate = HKQuery.predicateForWorkouts(with: .cycling)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate,
+                                              ascending: false)
+        let query = HKSampleQuery(sampleType: type,
+                                  predicate: predicate,
+                                  limit: HKObjectQueryNoLimit,
+                                  sortDescriptors: [sortDescriptor]) { _, samples, error in
+            guard let workouts = samples as? [HKWorkout],
+                  error == nil else {
+                return
+            }
+            self.workouts = workouts.sorted(by: { (lw, rw) -> Bool in
+                lw.startDate < rw.startDate
+            })
+        }
+        healthStore?.execute(query)
     }
 
 }
