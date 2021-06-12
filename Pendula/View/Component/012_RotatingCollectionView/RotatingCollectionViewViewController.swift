6ba14029544733ba0ecb482aeb91b32badd3494f
tokizuoh <k_sakai@yumemi.co.jp>
2021-06-12 10:12:33 +0900

    [#79] 012コンポーネントの枠組みと遷移を追加

---
+++ Pendula/View/Component/012_RotatingCollectionView/RotatingCollectionViewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RotatingCollectionViewViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/06/12.
+//
+
+import UIKit
+
+final class RotatingCollectionViewViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigationItem(navigationTitle: "012 RotatingCollectionView",
+                                blogURL: nil,
+                                githubPRURL: nil)
+    }
+
+}
