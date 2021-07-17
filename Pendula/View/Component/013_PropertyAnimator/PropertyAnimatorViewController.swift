a7939837ee68f2e53beb3f275259ab8f88063b62
tokizuoh <k_sakai@yumemi.co.jp>
2021-07-17 16:59:52 +0900

    [#82] アニメーション開始処理を追加

--- Pendula/View/Component/013_PropertyAnimator/PropertyAnimatorViewController.swift
+++ Pendula/View/Component/013_PropertyAnimator/PropertyAnimatorViewController.swift
@@ -11,20 +11,38 @@ final class PropertyAnimatorViewController: ComponentBaseViewController {
 
     @IBOutlet weak var squareView: UIView!
 
+    private var animator: UIViewPropertyAnimator!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationItem(navigationTitle: "013 PropertyAnimator",
                                 blogURL: nil,
                                 githubPRURL: nil)
+        configureAnimation()
     }
 
     @IBAction func startAnimation(_ sender: Any) {
-        // TODO [#82]: アニメーション開始処理
+        animator.startAnimation()
+    }
+
+    @IBAction func resetAnimation(_ sender: Any) {
+        // TODO [#82]: アニメーションリセット処理
     }
 
     @IBAction func stopAnimation(_ sender: Any) {
         // TODO [#82]: アニメーション停止処理
+    }
+
+}
+
+extension PropertyAnimatorViewController {
+
+    private func configureAnimation() {
+        animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut, animations: {
+            // 四角UIViewを画面右端まで移動させる
+            self.squareView.center.x += self.view.bounds.width / 2 - self.squareView.bounds.width / 2
+        })
     }
 
 }
