54b8e48a8a748ae33fbc603c65fdf38dc975f8d4
tokizuoh <tokizo819@gmail.com>
2021-09-11 13:36:09 +0900

    [#86] 通信処理をpresenterに切り分けた

--- Pendula/View/Component/014_DianthusMVP/Presenter/DianthusPresenter.swift
+++ Pendula/View/Component/014_DianthusMVP/Presenter/DianthusPresenter.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+enum DianthusError: Error {
+    case unknown
+}
+
+protocol DianthusPresenterProtocol {
+    func fetchWordList(from: String) -> Result<[String], DianthusError>
+}
+
+struct DianthusPresenter: DianthusPresenterProtocol {
+    func fetchWordList(from: String) -> Result<[String], DianthusError> {
+        let session = URLSession.shared
+        guard var urlComponents = URLComponents(string: "http://localhost:8080/v1/roman") else {
+            return .failure(.unknown)
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "target", value: from)
+        ]
+
+        var request = URLRequest(url: urlComponents.url!)
+        request.httpMethod = "GET"
+
+        let plistManager = PlistManager(fileName: "basic")
+        guard let username = plistManager?.getValue(key: "username"),
+              let password = plistManager?.getValue(key: "password") else {
+            return .failure(.unknown)
+        }
+
+        guard let basicAuthenticationData = "\(username):\(password)".data(using: .utf8) else {
+            return .failure(.unknown)
+        }
+
+        let basicAuthentication = basicAuthenticationData.base64EncodedString()
+        let basicData = "Basic \(basicAuthentication)"
+
+        request.setValue(basicData, forHTTPHeaderField: "Authorization")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        session.dataTask(with: request) { (_, _, _) in
+            // TODO [#86]: 変換する
+        }.resume()
+
+        return .success([])
+    }
+}
--- Pendula/View/Component/014_DianthusMVP/View/DianthusMVPViewController.swift
+++ Pendula/View/Component/014_DianthusMVP/View/DianthusMVPViewController.swift
@@ -9,18 +9,17 @@ import UIKit
 
 final class DianthusMVPViewController: ComponentBaseViewController {
 
+    var presenter: DianthusPresenterProtocol?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItem(navigationTitle: "014 Dianthus by MVP",
                                 blogURL: nil,
                                 githubPRURL: nil)
-        fetch()
-    }
 
-    private func fetch() {
-        let plistManager = PlistManager(fileName: "basic")
-        print(plistManager?.getValue(key: "username"))
-        print(plistManager?.getValue(key: "password"))
+        // TODO [#86]: Builderかなにかを作ってそこでDIする
+        presenter = DianthusPresenter()
+        presenter?.fetchWordList(from: "hoge")
     }
 
 }
