ff1a17462c408347c0d71f9481e8a702456b91b5
tokizuoh <tokizo819@gmail.com>
2021-09-11 10:07:51 +0900

    [#86] 014コンポーネントの枠組みと遷移を追加

---
+++ Pendula/View/Component/014_DianthusMVP/DianthusMVPViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DianthusMVPViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/09/11.
+//
+
+import UIKit
+
+final class DianthusMVPViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationItem(navigationTitle: "014 Dianthus by MVP",
+                                blogURL: nil,
+                                githubPRURL: nil)
+    }
+
+}
