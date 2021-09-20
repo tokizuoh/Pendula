362d19ac3c7c4e9e2b0e767a304e04ea3f9ba6fd
tokizuoh <tokizo819@gmail.com>
2021-09-20 23:03:25 +0900

    [#92] 入れ替え処理を追加

--- Pendula/View/Component/015_UICollectionViewFlowLayout/FlowLayoutViewController.swift
+++ Pendula/View/Component/015_UICollectionViewFlowLayout/FlowLayoutViewController.swift
@@ -11,14 +11,18 @@ final class FlowLayoutViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
-    private let colors: [UIColor] = [
+    private var colors: [UIColor] = [
         .red, .blue, .yellow,
         .brown, .black, .cyan,
         .green, .orange, .purple
     ]
 
+    private var transparencies: [CGFloat] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        transparencies = [Int](1..<10).map { CGFloat($0) / 10 }
 
         configureCollectionView()
     }
@@ -28,21 +32,47 @@ final class FlowLayoutViewController: UIViewController {
 extension FlowLayoutViewController {
 
     private func configureCollectionView() {
-        func configureLayout() -> UICollectionViewLayout {
-            let itemSpacing: CGFloat = 20
-            let itemCountPerRow: CGFloat = 3
-            let cellWidth: CGFloat = (view.bounds.width - (itemCountPerRow - 1) * itemSpacing) / itemCountPerRow - 0.1
-            let cellSize = CGSize(width: cellWidth, height: cellWidth)
-
-            let layout = UICollectionViewFlowLayout()
-            layout.itemSize = cellSize
-            layout.minimumLineSpacing = itemSpacing
-            layout.minimumInteritemSpacing = itemSpacing
-            return layout
-        }
-
         collectionView.dataSource = self
-        collectionView.collectionViewLayout = configureLayout()
+        configureLayout()
+        configureGestureRecognizer()
+    }
+
+    private func configureLayout() {
+        let itemSpacing: CGFloat = 20
+        let itemCountPerRow: CGFloat = 3
+        let cellWidth: CGFloat = (view.bounds.width - (itemCountPerRow - 1) * itemSpacing) / itemCountPerRow - 0.1
+        let cellSize = CGSize(width: cellWidth, height: cellWidth)
+
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = cellSize
+        layout.minimumLineSpacing = itemSpacing
+        layout.minimumInteritemSpacing = itemSpacing
+        collectionView.collectionViewLayout = layout
+    }
+
+    private func configureGestureRecognizer() {
+        let gesture = UILongPressGestureRecognizer(target: self,
+                                                   action: #selector(handleLongPressGesture(_:)))
+        collectionView.addGestureRecognizer(gesture)
+    }
+
+    @objc private func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            guard let indexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
+                return
+            }
+            collectionView.beginInteractiveMovementForItem(at: indexPath)
+
+        case .changed:
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
+
+        case .ended:
+            collectionView.endInteractiveMovement()
+
+        default:
+            collectionView.cancelInteractiveMovement()
+        }
     }
 
 }
@@ -50,14 +80,22 @@ extension FlowLayoutViewController {
 extension FlowLayoutViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return colors.count
+        return transparencies.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: R.reuseIdentifier.flowLayoutCell.identifier,
                                                       for: indexPath)
-        cell.backgroundColor = colors[indexPath.row]
+        cell.backgroundColor = .init(red: 0,
+                                     green: 0,
+                                     blue: 1.0,
+                                     alpha: transparencies[indexPath.row])
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = transparencies.remove(at: sourceIndexPath.row)
+        transparencies.insert(item, at: destinationIndexPath.row)
     }
 
 }
