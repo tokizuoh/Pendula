c03f05be60bcdb1714616d9d6bfa2eab7dce73eb
tokizuoh <tokizo819@gmail.com>
2021-11-21 15:58:50 +0900

    [#101] 画像のはめ込み

--- Pendula/View/Component/017_ClipPicture/ClipPictureViewController.swift
+++ Pendula/View/Component/017_ClipPicture/ClipPictureViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 final class ClipPictureViewController: ComponentBaseViewController {
 
+    @IBOutlet weak var baseImageView: UIImageView! {
+        didSet {
+            baseImageView.image = R.image.clip_picture_fugaku()
+        }
+    }
+    @IBOutlet weak var clippedImageView: UIImageView! {
+        didSet {
+            clippedImageView.backgroundColor = .lightGray
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItem(navigationTitle: "017 Clip Picture")
