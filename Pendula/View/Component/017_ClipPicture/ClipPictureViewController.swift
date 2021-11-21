a9f483ca1c1d0b97828f4836546adbd9e6e6f6fc
tokizuoh <tokizo819@gmail.com>
2021-11-21 16:31:12 +0900

    [#101] 切り抜きのロジックを追加

--- Pendula/View/Component/017_ClipPicture/ClipPictureViewController.swift
+++ Pendula/View/Component/017_ClipPicture/ClipPictureViewController.swift
@@ -14,10 +14,11 @@ final class ClipPictureViewController: ComponentBaseViewController {
             baseImageView.image = R.image.clip_picture_fugaku()
         }
     }
-    
+
     @IBOutlet weak var clippedImageView: UIImageView! {
         didSet {
             clippedImageView.backgroundColor = .lightGray
+            clippedImageView.contentMode = .scaleAspectFill
         }
     }
 
@@ -26,4 +27,30 @@ final class ClipPictureViewController: ComponentBaseViewController {
         configureNavigationItem(navigationTitle: "017 Clip Picture")
     }
 
+    @IBAction func clip(_ sender: Any) {
+        guard let targetImage = baseImageView.image,
+              let targetCGImage = targetImage.cgImage else {
+            return
+        }
+
+        let clippingWidth = clippedImageView.frame.width
+        let clippingHeight = clippedImageView.frame.height
+
+        let availableX = targetImage.size.width - clippingWidth
+        let availableY = targetImage.size.height - clippingHeight
+
+        let x = CGFloat.random(in: 0..<availableX)
+        let y = CGFloat.random(in: 0..<availableY)
+
+        let clippingRect = CGRect(x: x,
+                                  y: y,
+                                  width: x + clippingWidth,
+                                  height: y + clippingHeight)
+
+        guard let clippedCGImage = targetCGImage.cropping(to: clippingRect) else {
+            return
+        }
+        let crippedImage = UIImage(cgImage: clippedCGImage)
+        clippedImageView.image = crippedImage
+    }
 }
