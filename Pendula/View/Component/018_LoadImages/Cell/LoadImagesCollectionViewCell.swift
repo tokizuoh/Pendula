4a95c641832c7a3bf52f1e24022391872cfad6fd
tokizuoh <tokizo819@gmail.com>
2021-11-26 23:02:13 +0900

    [#104] collectionViewとcollectionViewCellを追加

---
+++ Pendula/View/Component/018_LoadImages/Cell/LoadImagesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LoadImagesCollectionViewCell.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/11/26.
+//
+
+import UIKit
+
+final class LoadImagesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+
+}
--- Pendula/View/Component/018_LoadImages/LoadImagesViewController.swift
+++ Pendula/View/Component/018_LoadImages/LoadImagesViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 final class LoadImagesViewController: ComponentBaseViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.dataSource = self
+            collectionView.register(R.nib.loadImagesCollectionViewCell)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItem(navigationTitle: "018 LoadImages")
+    }
+
+}
+
+extension LoadImagesViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: R.reuseIdentifier.loadImagesCollectionViewCell,
+                                                      for: indexPath)!
+        return cell
     }
 
 }
