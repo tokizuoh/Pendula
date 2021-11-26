ca8d91806c576bc37fe990266cca557a4f025146
tokizuoh <tokizo819@gmail.com>
2021-11-26 22:45:21 +0900

    [#104] 018コンポーネントの枠組みと遷移を追加

---
+++ Pendula/View/Component/018_LoadImages/LoadImagesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImagesViewController.swift
+//  Pendula
+//
+//  Created by tokizo on 2021/11/26.
+//
+
+import UIKit
+
+final class LoadImagesViewController: ComponentBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationItem(navigationTitle: "018 LoadImages")
+    }
+
+}
